735b45e022c243785ed24ed084847b79f3720de7
Thomas Moors <thomasmoors1992@gmail.com>
2016-04-13 14:09:51 +0200

    init

---
+++ gmailDemo/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using System.IO;
+using System.Net.Mail;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Gmail.v1.Data;
+using Message = Google.Apis.Gmail.v1.Data.Message;
+
+namespace gmailDemo
+{
+    public partial class Form1 : Form
+    {
+        private GmailService gs;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Form1(GmailService gs)
+        {
+            InitializeComponent();
+            this.gs = gs;
+        }
+
+        private void toonEmail(GmailService gs)
+        {
+            UsersResource.MessagesResource.ListRequest request2 = gs.Users.Messages.List("me");
+            IList<Google.Apis.Gmail.v1.Data.Message> mails = request2.Execute().Messages;
+            textBox4.Text = "";
+            foreach (var mail in mails)
+            {
+                textBox4.Text += mail.Id + "\n";
+            }
+           // textBox4.Text = mails.ToString();
+        }
+
+        private void zendEmail(GmailService gs, string to, string sub, string body)
+        {
+            var msg = new AE.Net.Mail.MailMessage
+            {
+                Subject = textBox2.Text,
+                Body = textBox3.Text,
+                From = new MailAddress("placeholder@gmail.com")
+            };
+            msg.To.Add(new MailAddress(textBox1.Text));
+            msg.ReplyTo.Add(msg.From); // Bounces without this!!
+            var msgStr = new StringWriter();
+            msg.Save(msgStr);
+
+          
+            var result = gs.Users.Messages.Send(new Message
+            {
+                Raw = Base64UrlEncode(msgStr.ToString())
+            }, "me").Execute();
+            Console.WriteLine("Message ID {0} sent.", result.Id);
+
+        }
+
+        private static string Base64UrlEncode(string input)
+        {
+            var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            // Special "url-safe" base64 encode.
+            return Convert.ToBase64String(inputBytes)
+              .Replace('+', '-')
+              .Replace('/', '_')
+              .Replace("=", "");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            zendEmail(gs, textBox1.Text, textBox2.Text, textBox3.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            toonEmail(gs);
+        }
+    }
+
+}
---
+++ gmailDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Gmail.v1;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace gmailDemo
+{
+    static class Program
+    {
+        // If modifying these scopes, delete your previously saved credentials
+        // at ~/.credentials/gmail-dotnet-quickstart.json
+        static string[] Scopes = { GmailService.Scope.GmailReadonly, GmailService.Scope.GmailSend };
+        static string ApplicationName = "Acousoft";
+
+        static void Main(string[] args)
+        {
+            UserCredential credential;
+
+            using (var stream = new FileStream("client_secret.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal);
+                credPath = Path.Combine(credPath, ".credentials/gmail-dotnet-quickstart.json");
+
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+
+            // Create Gmail API service.
+            GmailService service = new GmailService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = ApplicationName,
+            });
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1(service));
+        }
+    }
+}
